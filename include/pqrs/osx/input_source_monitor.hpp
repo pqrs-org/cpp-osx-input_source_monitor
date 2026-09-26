2098674e37b773a01f8d1e0a8e529cdb9e259865
Takayama Fumihiko <tekezo@pqrs.org>
2026-09-26 19:59:24 +0900

    v1.5.0

--- include/pqrs/osx/input_source_monitor.hpp
+++ include/pqrs/osx/input_source_monitor.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::input_source_monitor v1.4.0
+// pqrs::osx::input_source_monitor v1.5.0
 
 // (C) Copyright Takayama Fumihiko 2019.
 // Distributed under the Boost Software License, Version 1.0.
