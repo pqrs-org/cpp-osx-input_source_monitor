181d3b24e14a44bc29819a77bb5a7a0e487efce1
Takayama Fumihiko <tekezo@pqrs.org>
2026-09-26 19:59:14 +0900

    Use dispatcher_client_constructor_exception_guard

--- include/pqrs/osx/input_source_monitor.hpp
+++ include/pqrs/osx/input_source_monitor.hpp
@@ -17,6 +17,10 @@
 
 namespace pqrs::osx {
 class input_source_monitor final : public dispatcher::extra::dispatcher_client {
+private:
+  // Keep the guard first so member initialization failures also detach.
+  pqrs::dispatcher::extra::dispatcher_client_constructor_exception_guard dispatcher_client_constructor_exception_guard_{*this};
+
 public:
   // Signals (invoked from the dispatcher thread)
 
@@ -29,6 +33,7 @@ public:
   explicit input_source_monitor(std::weak_ptr<dispatcher::dispatcher> weak_dispatcher)
       : dispatcher_client(std::move(weak_dispatcher)),
         started_(false) {
+    dispatcher_client_constructor_exception_guard_.initialize();
   }
 
   ~input_source_monitor() override {
